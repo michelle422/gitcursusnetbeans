acf62f90edd1e24ba1bf3ae7cd5d05b60e0895c0
Michelle <michelle.norico@vdabcampus.be>
2017-10-20 14:34:03 +0200

    bonjour tout le monde

--- src/be/vdab/Gitcursus.java
+++ src/be/vdab/Gitcursus.java
@@ -7,7 +7,7 @@ package be.vdab;
 public class Gitcursus {
 
     public static void main(String[] args) {
-        System.out.println("Bonjour");
+        System.out.println("Bonjour tout le monde");
     }
     
 }
